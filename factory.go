36562d9a22c1562ac86633ff1369e08cdf6e370d
oyevikram <vikrambhaijee6@gmail.com>
2024-12-07 19:57:13 +0530

    Update factory.go

--- factory.go
+++ factory.go
@@ -1,8 +1,4 @@
-// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
 package vm
-
 import (
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms"
